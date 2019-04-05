9ca031ecdcdb72c050d407c68682001f199463d7
Toms Kusiņš <t.kusins@gmail.com>
2019-04-05 20:53:52 +0300

    Task 7 extra

---
+++ task7extra/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enter compressed signature:\n> ");
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String decompressed;
+        boolean markersFound;
+
+        do {
+            markersFound = false;
+            decompressed = "";
+
+            for (int cur = 0; cur < text.length(); cur++) {
+                char symbol = text.charAt(cur);
+                if (symbol != '(') {
+                    decompressed += symbol;
+                    continue;
+                }
+
+                markersFound = true;
+                String marked = text.substring(cur);
+                int xIndex = marked.indexOf('x');
+                int closingIndex = marked.indexOf(')');
+                String len = marked.substring(1, xIndex);
+                String mul = marked.substring(xIndex + 1, closingIndex);
+
+                int segmentLen = Integer.parseInt(len);
+                int segmentTimes = Integer.parseInt(mul);
+
+                String segment = marked.substring(closingIndex + 1, closingIndex + 1 + segmentLen);
+
+                for (int i = 0; i < segmentTimes; i++) {
+                    decompressed += segment;
+                }
+
+                cur += segmentLen + len.length() + mul.length() + 2;
+            }
+            text = decompressed;
+        } while (markersFound);
+
+        System.out.println("The decompressed signature:\n" + decompressed);
+    }
+}
